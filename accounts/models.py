7b29172235ebeba30c51775f6b84f68027a84be9
Kiuliumov <miti.bania@gmail.com>
2026-05-31 10:52:42 +0300

    Initial

---
+++ accounts/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+# Create your models here.
+
+
+class User(AbstractUser):
+    pass
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_picture_url = models.URLField(blank=True, null=True)
+    bio = models.TextField(blank=True, null=True)
+
+
+
